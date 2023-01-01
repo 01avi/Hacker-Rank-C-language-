af9cf7a60c7d0bf70094aa1d000382d7c3fbfeb1
Aviral <110219413+01avi@users.noreply.github.com>
2023-01-01 23:16:40 +0530

    Y

--- Function in c.c
+++ Function in c.c
@@ -1,7 +1,7 @@
 //Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them
 
 
-#include <stdio.h>
+#include <stdio.h>y
 int max_of_four(int a, int b, int c, int d);
 int main() {
     int a, b, c, d;
