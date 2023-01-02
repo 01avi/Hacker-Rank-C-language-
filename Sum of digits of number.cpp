b01d30be59522aa2422109b055c1f98d56b1a6e0
Aviral <110219413+01avi@users.noreply.github.com>
2023-01-02 20:11:40 +0530

    HACKER RANK PROBLEMS

---
+++ Sum of digits of number.cpp
@@ -0,0 +1,20 @@
+//Given a five digit integer, print the sum of its digits.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,m,sum=0;
+    scanf("%d", &n);
+    while(n>0)    
+    {    
+        m=n%10;    
+        sum=sum+m;     
+        n=n/10;    
+    }     
+    printf("%d",sum);   
+    return 0;
+}
